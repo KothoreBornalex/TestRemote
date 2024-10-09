2a9e43750c0dce63579047cea75c11f1af3e5ae1
Ibrahim-Mallouka <panda.bornalex@gmail.com>
2024-10-09 14:25:56 +0200

    Initial Commit

---
+++ Source/DevRPGFeatures/DevRPGFeaturesGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DevRPGFeaturesGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADevRPGFeaturesGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADevRPGFeaturesGameMode();
+};
+
+
+
